94b217a440a29d331c9e9fc9e944747e02f693af
nicolas pineault <npineault@fungo.ca>
2020-02-10 12:39:38 -0500

    use complete state module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default as MyThemeProvider } from './MyThemeProvider'
 export { default as useUiContext } from './useUiContext'
 export { default as uiUses } from './uses'
+export { default as useCompleteState } from './useCompleteState'
+
