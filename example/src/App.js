436bd6b307acf7886a851c01effadfe71751eda7
nicolas pineault <npineault@fungo.ca>
2020-02-11 07:13:57 -0500

    cleanup

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,7 @@
 import React from 'react'
 
-//import { ThemeProvider, createMuiTheme } from '@material-ui/core'
-
 import { BaseThemeProvider } from 'testing'
 import AppBody from './AppBody'
-
-require('react-dom')
-window.React2 = require('react')
-
-console.log('App React2 version', window.React2.version)
-console.log('App react versions', window.React === window.React2)
 
 export default () => {
   return (
